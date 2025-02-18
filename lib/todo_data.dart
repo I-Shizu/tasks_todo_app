b9bb703247c55a56847ef1f246e452dd0585fb30
ikari shizuku <ikari2002pcs@icloud.com>
2025-02-19 02:15:57 +0900

    メソッドの追加

--- lib/todo_data.dart
+++ lib/todo_data.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Task {
   String id;
   String title;
@@ -24,6 +26,17 @@ class Task {
     );
   }
 
+  // Firestore用の変換メソッド
+  factory Task.fromDocument(DocumentSnapshot doc) {
+    return Task(
+      id: doc.id,
+      title: doc['title'],
+      deadline: (doc['deadline'] as Timestamp).toDate(),
+      priority: doc['priority'],
+      completed: doc['completed'],
+    );
+  }
+
   Map<String, dynamic> toFirestore() {
     return {
       'title': title,
@@ -31,5 +44,21 @@ class Task {
       'priority': priority,
       'completed': completed,
     };
+  }
+
+  // タスクのコピーを作成するメソッド
+  Task copyWith({
+    String? title,
+    DateTime? deadline,
+    int? priority,
+    bool? completed,
+  }) {
+    return Task(
+      id: id,
+      title: title ?? this.title,
+      deadline: deadline ?? this.deadline,
+      priority: priority ?? this.priority,
+      completed: completed ?? this.completed,
+    );
   }
 }
