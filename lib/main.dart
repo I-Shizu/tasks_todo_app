67f285338941c319ef9348bf82591d2b38762a80
ikari shizuku <ikari2002pcs@icloud.com>
2025-03-10 14:12:35 +0900

    認証状態によってstreamで表示させるページを変えるようにする

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'Data/firebase_options.dart';
 
 import '../UI/login_screen.dart';
+import 'UI/home_page.dart';
 import 'providers.dart';
 
 Future<void> main() async {
@@ -24,6 +26,7 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isDark = ref.watch(themeNotifierProvider);
+    final authCheck = FirebaseAuth.instance.authStateChanges();
 
     return MaterialApp(
       theme: ThemeData(
@@ -43,7 +46,21 @@ class MyApp extends ConsumerWidget {
       ),
       darkTheme: ThemeData.dark(),
       themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
-      home: LoginScreen(),
+      home: StreamBuilder<User?>(
+        stream: authCheck,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // スプラッシュ画面などに書き換えても良い
+            return const SizedBox();
+          }
+          if (snapshot.hasData) {
+            // User が null でなない、つまりサインイン済みのホーム画面へ
+            return HomePage();
+          }
+          // User が null である、つまり未サインインのサインイン画面へ
+          return LoginScreen();
+        },
+      ),
     );
   }
 }
